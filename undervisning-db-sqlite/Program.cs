b582e754c9b692f73266c589a8eb07ff63384bbb
Michell Cronberg <michell@cronberg.dk>
2019-09-24 22:06:38 +0200

    program

--- undervisning-db-sqlite/Program.cs
+++ undervisning-db-sqlite/Program.cs
@@ -14,30 +14,30 @@ namespace undervisning_db_sqlite
 
             var path = @"C:\temp\undervisning-db-sqlite\undervisning-db-sqlite\Data\people.db";
 
-// Get people
-using (PeopleContext c = new PeopleContext(path))
-{
-    var res = c.People.Where(i => i.Height > 170 && i.IsHealthy).OrderBy(i => i.LastName).ToList();
-    res.ForEach(i => Console.WriteLine(i));
-}
+            // people
+            using (PeopleContext c = new PeopleContext(path))
+            {
+                var res = c.People.Where(i => i.Height > 170 && i.IsHealthy).OrderBy(i => i.LastName).ToList();
+                res.ForEach(i => Console.WriteLine(i));
+            }
 
-// Get countries with navigation property
-using (PeopleContext c = new PeopleContext(path))
-{
-    var res = c.Countries.Include(i => i.People).ToList();
-    res.ForEach(i =>
-    {
-        Console.WriteLine(i.Name);
-        i.People.ForEach(x => Console.WriteLine("\t" + x));
-    });
-}
+            // navigation property
+            using (PeopleContext c = new PeopleContext(path))
+            {
+                var res = c.Countries.Include(i => i.People).ToList();
+                res.ForEach(i =>
+                {
+                    Console.WriteLine(i.Name);
+                    i.People.ForEach(x => Console.WriteLine("\t" + x));
+                });
+            }
 
-// Get countries with join
-using (PeopleContext c = new PeopleContext(path))
-{
-    var res = from country in c.Countries orderby country.CountryId join person in c.People on country.CountryId equals person.CountryId select new { person.FirstName, person.LastName, country.Name };
-    res.ToList().ForEach(i => Console.WriteLine(i.FirstName + " " + i.LastName + " from " + i.Name));
-}
+            // join
+            using (PeopleContext c = new PeopleContext(path))
+            {
+                var res = from country in c.Countries orderby country.CountryId join person in c.People on country.CountryId equals person.CountryId select new { person.FirstName, person.LastName, country.Name };
+                res.ToList().ForEach(i => Console.WriteLine(i.FirstName + " " + i.LastName + " from " + i.Name));
+            }
 
 
         }
